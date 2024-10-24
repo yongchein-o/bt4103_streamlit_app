ea7127b167a25c7cb178cf3da8d2b6f771710331
Yong Chein <yongchein.o@gmail.com>
2024-10-24 11:38:27 +0000

    Implement Noel's idea

--- settings.py
+++ settings.py
@@ -1,4 +1,98 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+import hashlib
+import os
 
-st.header("Selections")
-st.write(f"Your selections are {st.session_state.selections}.")
+# st.header("Selections")
+# st.write(f"Your selections are {st.session_state.selections}.")
+
+# def columns_array_creator(total_number, max_column_per_row):
+#     columns_array = []
+#     count = 0
+#     while(count + max_column_per_row < total_number):
+#         count += max_column_per_row
+#         columns_array.append(max_column_per_row)
+#     if(total_number - count > 0):
+#         columns_array.append(total_number - count)
+#     return columns_array
+
+# selections = st.session_state.selections
+# unique_values = set(item.split(" - ")[0] for item in selections)
+# unique_count = len(unique_values)
+
+# outer_columns_array = columns_array_creator(unique_count, 2)
+# st.write(outer_columns_array)
+
+@st.cache_data
+def load_data(file_path='data.xlsx'):
+    return pd.read_excel(file_path, sheet_name='data')
+
+df = load_data()
+
+
+
+def create_line_chart(parent_container, key):
+    child_container = parent_container.container()
+    col1, col2 = child_container.columns(2)
+    x_axis = col1.selectbox(label="X axis variable", options=["CreateDate", "Price"], index=0, key=key+"line_x_axis")
+    y_axis = col2.selectbox(label="Y axis variable", options=["Value", "Quantity"], index=0, key=key+"line_y_axis")
+    fig = px.line(df, x=x_axis, y=y_axis, title=f'Line Chart of {y_axis} vs {x_axis}')
+    child_container.plotly_chart(fig, key=key+"line")
+
+
+def create_pie_chart(parent_container, key):
+    child_container = parent_container.container()
+    #col1, col2 = child_container.columns(2)
+    #variable = col1.selectbox(label="Variable", options=["Order Capacity",], index=0)
+    variable = child_container.selectbox(label="Variable", options=["OrderCapacity", "Exchange"], index=0, key=key+"pie_var")
+    fig = px.pie(df, values='Price', names=variable, title=f'Pie Chart of {variable}')
+    child_container.plotly_chart(fig, key=key+"pie")
+    
+
+col1, col2 = st.columns(2)
+chart_type_1 = col1.selectbox(label="Chart type", options=["Line Chart", "Pie Chart"], index=0)
+chart_type_2 = col2.selectbox(label="Chart type", options=["Line Chart", "Pie Chart"], index=1)
+
+match chart_type_1:
+    case "Line Chart": create_line_chart(col1, "1")
+    case "Pie Chart": create_pie_chart(col1, "1")
+
+match chart_type_2:
+    case "Line Chart": create_line_chart(col2, "2")
+    case "Pie Chart": create_pie_chart(col2, "2")
+
+    
+
+
+# col1, col2, col3 = st.columns(3)  # Columns are containers
+# # overview_container = st.container(border=True, key="overview_container")
+# metrics_container = st.container(key="metrics_container")
+
+
+# overview_container = col1.container(border=True)
+# overview_expander = overview_container.expander("More Details")
+# overview_expander.write("ZM insert your report here")
+
+
+# with col1:
+    
+
+
+#     # with overview_container:
+#     with metrics_container:
+#         col_a, col_b, col_c = st.columns(3)
+#         with col_a:
+#             st.metric("Total Number of Orders", "5K")
+
+#         with col_b:
+#             st.metric("Total Volume of Orders", "40.39M")
+
+#         with col_c:
+#             st.metric("Total Value of Orders", "$33.28M")
+#     with st.expander("More Details"):
+#         st.write('''
+#             ZM insert your report here.
+#         ''')
+
+            
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,35 @@
 import streamlit as st
+import datetime
 
 st.set_page_config(page_title="My App", layout="wide")
 
 SELECTIONS = [
-    "Business Intelligence: Overview - Statistics",
-    "Business Intelligence: Overview - BUY/SELL By SEC",
-    "Business Intelligence: Overview - SEC to BUY/SELL Sankey",
-    "Business Intelligence: Order Details - Capacity",
-    "Business Intelligence: Order Details - Lifetime",
-    "Business Intelligence: Order Details - Price Instruction",
-    "Business Intelligence: Time Series - Trades Created Over Time",
-    "Anomaly Detection: Plot 1",
-    "Anomaly Detection: Plot 2",
-    "Anomaly Detection: Plot 3",
-    "ASIC Reporting: Plot 1",
-    "ASIC Reporting: Plot 2",
-    "ASIC Reporting: Plot 3",
-    "Future Functionality: Plot 1",
-    "Future Functionality: Plot 2",
-    "Future Functionality: Plot 3",
-    "Future Functionality: Plot 4",
+    "BI: Overview - Statistics",
+    "BI: Overview - BUY/SELL By SEC",
+    "BI: Overview - SEC to BUY/SELL Sankey",
+    "BI: Order Details - Capacity",
+    "BI: Order Details - Lifetime",
+    "BI: Order Details - Price Instruction",
+    "BI: Time Series - Number Of Trades Over Time",
+    "BI: Time Series - Value Of Trades Over Time",
+    "Anomaly Detection: Container 1 - Plot 1",
+    "Anomaly Detection: Container 1 - Plot 2",
+    "Anomaly Detection: Container 1 - Plot 3",
+    "ASIC Reporting: Container 1 - Plot 1",
+    "ASIC Reporting: Container 1 - Plot 2",
+    "ASIC Reporting: Container 1 - Plot 3",
+    "Future Functionality: Container 1 - Plot 1",
+    "Future Functionality: Container 1 - Plot 2",
+    "Future Functionality: Container 2 - Plot 1",
+    "Future Functionality: Container 2 - Plot 2",
 ]
 
 # Initialize selections as an empty list in session state
 if "selections" not in st.session_state:
     st.session_state.selections = []
-
+# Initialize date as None in session state
+if "date" not in st.session_state:
+    st.session_state.date = None
 
 def update_multiselect_style():
     st.markdown(
@@ -41,47 +45,54 @@ def update_multiselect_style():
         """,
         unsafe_allow_html=True,
     )
-
-
 update_multiselect_style()
 
 
 def login():
     st.header("Dashboard View Customization")
+    today = datetime.datetime.now()
+    date = st.date_input("Select the date for the report", max_value = today, value=None)
     selections = st.multiselect(
-        "What are your desired charts",
+        "Select the desired chart(s)",
         SELECTIONS,
         placeholder="Nothing selected yet",
     )
     if st.button("Confirm", icon=":material/arrow_right_alt:"):
         st.session_state.selections = selections
+        st.session_state.date = date
         st.rerun()
 
 
 def logout():
     st.session_state.selections = []
+    st.session_state.date = None
     st.rerun()
 
-
+# Declare selections and conn
 selections = st.session_state.selections
+date = st.session_state.date
+
+# conn = st.connection("my_database")
+# df = conn.query("select * from trial")
+# st.dataframe(df)
 
 logout_page = st.Page(
     logout,
     title="Return to Chart Selections",
     icon=":material/logout:",
-    default=(not bool(selections)),
+    default=(not bool(selections) or not bool(selections)),
 )
 dashboard_page = st.Page(
     "settings.py",
     title="Current Dashboard",
     icon=":material/visibility:",
-    default=(bool(selections)),
+    default=(bool(selections) and bool(date)),
 )
 
 
 # Define common element
 st.title("Capstone Project: End-of-Day Summary Dashboard")
-if selections:
+if selections and (date is not None):
     # Display logout option if selections are made
     pg = st.navigation(
         {
